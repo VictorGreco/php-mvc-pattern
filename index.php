b1d44e08c934d152fd98e903e0034d981f7ef322
victorgreco <victorgreco263@gmail.com>
2021-02-20 11:37:36 +0100

    update fillename and ddbb schemas

--- index.php
+++ index.php
@@ -8,3 +8,5 @@ include_once "config/constants.php";
 
 // TODO Implement the logic to include the controller passed by the URL dynamically
 // In the event that the controller passed by URL does not exist, you must show the error view.
+
+
---
+++ models/userModel.php
@@ -0,0 +1,16 @@
+<?php
+
+function create() {
+}
+
+function getAll() {
+}
+
+function getById($id) {
+}
+
+function updateById($id) {
+}
+
+function deleteById($id) {
+}
